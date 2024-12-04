3c4b9d11e39765034be2f0e5d3a0eb90b965cde0
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-12-04 21:33:40 +0100

    Dependency injection extension #69

---
+++ src/Trailblazor.Routing/DependencyInjection/RoutingDependencyInjection.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Trailblazor.Routing.Configuration;
+using Trailblazor.Routing.Configuration.Validation;
+using Trailblazor.Routing.Navigation;
+using Trailblazor.Routing.Parsing;
+
+namespace Trailblazor.Routing.DependencyInjection;
+
+/// <summary>
+/// Static class that contains routing related extension methods.
+/// </summary>
+public static class RoutingDependencyInjection
+{
+    /// <summary>
+    /// Adds services required for the <see cref="TrailblazorRouter"/> to function correctly to the given <paramref name="services"/>.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> being extended and gettint Trailblazor services registered to it.</param>
+    /// <param name="options"> Optional builder action for configuring <see cref="IRoutingOptions"/>. The configured options can later be accessed by injecting the <see cref="IRoutingOptionsProvider"/>.</param>
+    /// <returns>The <see cref="IServiceCollection"/> for further dependency injection configurations.</returns>
+    public static IServiceCollection AddTrailblazorRouting(this IServiceCollection services, Action<IRoutingOptionsBuilder>? options = null)
+    {
+        var optionsBuilder = new RoutingOptionsBuilder();
+        options?.Invoke(optionsBuilder);
+        var routingOptions = optionsBuilder.Build();
+
+        services.AddSingleton<IRoutingOptionsProvider>(sp => new RoutingOptionsProvider(routingOptions));
+        //services.AddSingleton<IRoutingConfigurationValidator, RoutingConfigurationValidator>();
+        services.AddScoped<IRoutingConfigurationResolver, RoutingConfigurationResolver>();
+        services.AddScoped<IRoutingConfigurationProvider, RoutingConfigurationProvider>();
+        //services.AddScoped<INodeProvider, NodeProvider>();
+        //services.AddScoped<IRouteNavigator, RouteNavigator>();
+        //services.AddScoped<IRouterContextProvider, RouterContextProvider>();
+        //services.AddSingleton<IUriParser, Parsing.UriParser>();
+        //services.AddSingleton<IComponentParameterParser, ComponentParameterParser>();
+
+        foreach (var routingProfileType in routingOptions.RoutingProfileTypes)
+            services.AddScoped(typeof(IRoutingProfile), routingProfileType);
+
+        return services;
+    }
+}
