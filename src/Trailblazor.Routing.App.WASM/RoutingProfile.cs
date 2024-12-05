effa5f01b1d9d4b603391c7fa9cc9708b6b28822
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-12-05 16:33:47 +0100

    WASM test app #69

---
+++ src/Trailblazor.Routing.App.WASM/RoutingProfile.cs
@@ -0,0 +1,17 @@
+﻿using Trailblazor.Routing.App.WASM.Pages;
+using Trailblazor.Routing.Configuration;
+
+namespace Trailblazor.Routing.App.WASM;
+
+internal sealed class RoutingProfile : IRoutingProfile
+{
+    public void ConfigureRoutes(IRoutingConfigurationBuilder builder)
+    {
+        builder.AddNode<Home>("Home", "/");
+        builder.AddNode("Content", g =>
+        {
+            g.AddNode<Counter>("Counter", "/counter");
+            g.AddNode<Weather>("Weather", "/weather");
+        });
+    }
+}
