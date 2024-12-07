af6031ec2419fba0819c09f0967468e510717fe6
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-12-07 20:49:43 +0100

    Unit tests assembly preparations #75

---
+++ src/Trailblazor.Routing.Tests/DI/RoutingServiceProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Trailblazor.Routing.DependencyInjection;
+
+namespace Trailblazor.Routing.Tests.DI;
+
+public static class TestServiceProviderFactory
+{
+    public static IServiceProvider Create(Action<IRoutingOptionsBuilder>? options = null)
+    {
+        return new ServiceCollection().AddTrailblazorRouting(options).BuildServiceProvider();
+    }
+}
---
+++ src/Trailblazor.Routing.Tests/Mocks/ComponentMock.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Trailblazor.Routing.Tests.Mocks;
+
+internal sealed class ComponentMock : ComponentBase
+{
+    [Parameter]
+    [QueryParameter("customStringParameter")]
+    public string? StringParameter { get; set; }
+
+    [Parameter]
+    [QueryParameter("customBoolParameter")]
+    public bool BoolParameter { get; set; }
+
+    [Parameter]
+    [QueryParameter("customGuidParameter")]
+    public Guid GuidParameter { get; set; }
+
+    [Parameter]
+    [QueryParameter("customTimeOnlyParameter")]
+    public TimeOnly TimeOnlyParameter { get; set; }
+
+    [Parameter]
+    [QueryParameter("customDateOnlyParameter")]
+    public DateOnly DateOnlyParameter { get; set; }
+
+    [Parameter]
+    [QueryParameter("customDateTimeParameter")]
+    public DateTime DateTimeParameter { get; set; }
+
+    [Parameter]
+    [QueryParameter("customIntParameter")]
+    public int IntParameter { get; set; }
+
+    [Parameter]
+    [QueryParameter("customDoubleParameter")]
+    public double DoubleParameter { get; set; }
+
+    [Parameter]
+    [QueryParameter("customLongParameter")]
+    public long LongParameter { get; set; }
+
+    [Parameter]
+    [QueryParameter("customDecimalParameter")]
+    public decimal DecimalParameter { get; set; }
+
+    [Parameter]
+    [QueryParameter("differentParameterName")]
+    public int ValidQueryParameterWithCustomName { get; set; }
+
+    [QueryParameter("missingParameterAttribute")]
+    public int InvalidQueryParameterWithMissingParameterAttribute { get; set; }
+}
